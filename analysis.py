d5b03a68403d55e0caf5ae6fb4e7c1e86e53a9bc
Kyleigh <candyrae2006@gmail.com>
2026-03-07 16:44:16 -0600

    Read and check quality of first data file

--- analysis.py
+++ analysis.py
@@ -1,13 +1,17 @@
-import pandas as panda
+import pandas as pd
 import matplotlib.pyplot as plot
 import csv
 
 def main():
     #1. Load exported data
-    data_file = panda.read_csv("Housing_Unit_Data.csv", parse_dates=["Date"])
+    data_file = pd.read_csv("Housing_Unit_Data.csv")
     #2. Inspect structure
-    print(data_file.info())
+    data_file.info()
+    data_file.head()
     #3. Check data quality
+    print(data_file.isna().sum())
+    print(data_file.describe())
     #4. Validate keys
+
 if __name__ == "__main__":
     main()
