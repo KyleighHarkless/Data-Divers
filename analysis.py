24c7433e42fd109591bafe446a8655a78190b939
Kyleigh <candyrae2006@gmail.com>
2026-03-07 16:33:39 -0600

    Read first file

--- analysis.py
+++ analysis.py
@@ -4,8 +4,9 @@ import csv
 
 def main():
     #1. Load exported data
-    data_file = panda.read_csv("Housing_Unit_Data.csv")
+    data_file = panda.read_csv("Housing_Unit_Data.csv", parse_dates=["Date"])
     #2. Inspect structure
+    print(data_file.info())
     #3. Check data quality
     #4. Validate keys
 if __name__ == "__main__":
