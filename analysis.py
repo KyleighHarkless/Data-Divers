13ed2b2c2fe4d02753d0796f97e90180b05a627d
Kyleigh <candyrae2006@gmail.com>
2026-03-07 16:28:31 -0600

    Create main and structure

--- analysis.py
+++ analysis.py
@@ -1,2 +1,12 @@
 import pandas as panda
 import matplotlib.pyplot as plot
+import csv
+
+def main():
+    #1. Load exported data
+    data_file = panda.read_csv("Housing_Unit_Data.csv")
+    #2. Inspect structure
+    #3. Check data quality
+    #4. Validate keys
+if __name__ == "__main__":
+    main()
