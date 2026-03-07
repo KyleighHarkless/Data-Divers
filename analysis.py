49174f77ef5948566e4a8e2a83b4c136d89ed45c
Kyleigh <candyrae2006@gmail.com>
2026-03-07 17:07:13 -0600

    Load more data tables into main

--- analysis.py
+++ analysis.py
@@ -6,6 +6,12 @@ def main():
     #1. Load exported data
     housing_data = pd.read_csv("Housing_Unit_Data.csv")
     report(housing_data)
+
+    income_data = pd.read_csv("Income_Data.csv")
+    report(income_data)
+
+    agesex_data = pd.read_csv("Age and Sex.csv")
+    report(agesex_data)
     
 def report(df):
     #2. Inspect structure
