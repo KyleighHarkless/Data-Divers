c228dc92eee4486e8ed61bd1f8f36d5d251143e8
Kyleigh <candyrae2006@gmail.com>
2026-03-07 17:09:43 -0600

    Remove print for df.head()

--- analysis.py
+++ analysis.py
@@ -6,13 +6,19 @@ def main():
     #1. Load exported data
     housing_data = pd.read_csv("Housing_Unit_Data.csv")
     report(housing_data)
+
+    income_data = pd.read_csv("Income_Data.csv")
+    report(income_data)
+
+    agesex_data = pd.read_csv("Age and Sex.csv")
+    report(agesex_data)
     
 def report(df):
     #2. Inspect structure
     rows = df.shape[0]
     columns = df.shape[1]
     print(f"Rows: {rows}, Columns: {columns}")
-    print(df.head()) # Display the first 5 rows of the DataFrame
+    df.head() # Display the first 5 rows of the DataFrame
     df.info() # Display summary information about the DataFrame, including data types and non-null counts
     
     #3. Check data quality
