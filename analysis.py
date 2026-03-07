e8bc12b7d20b30497705812017edc867388f1258
Kyleigh <candyrae2006@gmail.com>
2026-03-07 17:03:29 -0600

    Inspect structure

--- analysis.py
+++ analysis.py
@@ -4,13 +4,21 @@ import csv
 
 def main():
     #1. Load exported data
-    data_file = pd.read_csv("Housing_Unit_Data.csv")
+    housing_data = pd.read_csv("Housing_Unit_Data.csv")
+    report(housing_data)
+    
+def report(df):
     #2. Inspect structure
-    data_file.info()
-    data_file.head()
+    rows = df.shape[0]
+    columns = df.shape[1]
+    print(f"Rows: {rows}, Columns: {columns}")
+    print(df.head()) # Display the first 5 rows of the DataFrame
+    df.info() # Display summary information about the DataFrame, including data types and non-null counts
+    
     #3. Check data quality
-    print(data_file.isna().sum())
-    print(data_file.describe())
+    print("---------------------")
+    print(df.isna().sum())
+    print(df.describe())
     #4. Validate keys
 
 if __name__ == "__main__":
